e65f0580fda0c563e4e50d08575ca3ea9bfe57b6
Alom <posta@192.168.1.32>
2020-05-26 09:41:35 +0200

    start

---
+++ src/main/java/com/mycompany/esempio/startApp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.esempio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author posta
+ */
+public class startApp {
+
+    public static void main(String[] args) {
+
+        int[] valori = {5, 5, 5, 5, 5, 5, 6, 5, 5, 5};
+
+        boolean ok = true;
+        for (int x : valori) {
+            if (x != valori[0]) {
+                ok = false;
+            }
+        }
+
+        if (ok) {
+            System.out.println("tutti uguali");
+        } else {
+            System.out.println("non tutti uguali");
+        }
+
+    }
+
+}
